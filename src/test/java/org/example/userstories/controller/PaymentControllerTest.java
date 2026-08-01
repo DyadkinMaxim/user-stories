24e99334b2537986ece215bdf5c84fa0de4daf55
Dyadkin Maxim <maxim.dyadkin@sharp-dev.net>
2026-08-01 20:19:41 +0200

    fix import

--- src/test/java/org/example/userstories/controller/PaymentControllerTest.java
+++ src/test/java/org/example/userstories/controller/PaymentControllerTest.java
@@ -21,7 +21,6 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;

220ec27cc808fadb0c04ceb3e411d26853e02b33
Dyadkin Maxim <maxim.dyadkin@sharp-dev.net>
2026-08-05 13:41:48 +0200

    aspects audit tests

--- src/test/java/org/example/userstories/aspect/PaymentAspectTest.java
+++ src/test/java/org/example/userstories/aspect/PaymentAspectTest.java
@@ -1,5 +1,8 @@
 package org.example.userstories.aspect;
 
+import jakarta.persistence.EntityManager;
+import org.example.userstories.dto.PaymentUpdateRequest;
+import org.example.userstories.mapper.PaymentMapper;
 import org.example.userstories.model.Payment;
 import org.example.userstories.repository.PaymentRepository;
 import org.example.userstories.service.PaymentService;
@@ -9,10 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -24,14 +27,43 @@ public class PaymentAspectTest {
     @MockitoBean
     PaymentRepository paymentRepository;
 
+    @MockitoBean
+    EntityManager entityManager;
+
+    @MockitoBean
+    PaymentMapper paymentMapper;
+
     @Test
-    void save_doesnotThrowAspectException() {
+    void save_doesNotThrowAspectException() {
         Payment payment = new Payment();
         when(paymentRepository.findByIdempotencyKey(any()))
-                .thenReturn(Optional.empty());   // ← not duplicate ✅
+                .thenReturn(Optional.empty());
         when(paymentRepository.save(any()))
                 .thenReturn(payment);
 
-        assertDoesNotThrow(() -> paymentService.save(payment, eq("1")));
+        assertDoesNotThrow(() -> paymentService.save(payment, "1"));
+    }
+
+    @Test
+    void update_doesNotThrowAspectException() {
+        Payment payment = new Payment();
+        when(paymentRepository.findById(any()))
+                .thenReturn(Optional.of(payment));
+
+        assertDoesNotThrow(() -> paymentService.updatePaymentDetails(
+                UUID.randomUUID(), new PaymentUpdateRequest(
+                        200.0, "USD", "ACC-002",
+                        "DE89370400440532013001"
+                )));
+    }
+
+    @Test
+    void softDelete_doesNotThrowAspectException() {
+        Payment payment = new Payment();
+        when(paymentRepository.findById(any()))
+                .thenReturn(Optional.of(payment));
+
+        assertDoesNotThrow(() -> paymentService.softDelete(
+                UUID.randomUUID()));
     }
 }
